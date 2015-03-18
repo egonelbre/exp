a5f9a80fcd171128f81d1ba3676b1d52343ea69c
Egon Elbre <egonelbre@gmail.com>
2015-03-18 21:36:32 +0200

    Fix things

--- physicscompress/physics/encoding.go
+++ physicscompress/physics/encoding.go
@@ -5,7 +5,6 @@ import (
 	"compress/flate"
 
 	"github.com/egonelbre/exp/bit"
-	"github.com/egonelbre/exp/bit/expgolomb"
 )
 
 func encode32(v int32) uint64 { return uint64(bit.AbsEncode(int64(v))) }
@@ -28,43 +27,30 @@ func read32(r *bit.Reader, bits uint64) int32 {
 	return int32(bit.AbsDecode(v))
 }
 
-type bits struct {
-	ABC uint64
-	XYZ uint64
-}
-
-func (b *bits) WriteTo(w *bit.Writer) {
-	expgolomb.WriteInt(w, int(b.ABC)-0xA)
-	expgolomb.WriteInt(w, int(b.XYZ)-0xA)
-}
-
-func (b *bits) ReadFrom(r *bit.Reader) {
-	abc, _ := expgolomb.ReadInt(r)
-	xyz, _ := expgolomb.ReadInt(r)
-
-	b.ABC = uint64(abc + 0xA)
-	b.XYZ = uint64(xyz + 0xA)
-}
-
-func (b *bits) Update(baseline, current *Frame) {
-	for i, cube := range current.Cubes {
-		base := baseline.Cubes[i]
-		b.ABC = maxbits(b.ABC, cube.A^base.A)
-		b.ABC = maxbits(b.ABC, cube.B^base.B)
-		b.ABC = maxbits(b.ABC, cube.C^base.C)
-		b.XYZ = maxbits(b.XYZ, cube.X^base.X)
-		b.XYZ = maxbits(b.XYZ, cube.Y^base.Y)
-		b.XYZ = maxbits(b.XYZ, cube.Z^base.Z)
+func maxbits(vs ...int32) (r uint64) {
+	r = 0
+	for _, x := range vs {
+		if x != 0 {
+			bits := bit.ScanRight(bit.AbsEncode(int64(x))) + 4
+			if r < bits {
+				r = bits
+			}
+		}
 	}
+	return
 }
 
-func maxbits(a uint64, b int32) uint64 {
-	x := bit.AbsEncode(int64(b))
-	w := bit.ScanRight(x) + 1
-	if a < w {
-		return w
-	}
-	return a
+func deltabits(base, cube *Cube) uint64 {
+	return maxbits(
+		cube.Interacting^base.Interacting,
+		cube.Largest^base.Largest,
+		cube.A^base.A,
+		cube.B^base.B,
+		cube.C^base.C,
+		cube.X^base.X,
+		cube.Y^base.Y,
+		cube.Z^base.Z,
+	)
 }
 
 func (s *State) Encode() []byte {
@@ -76,23 +62,34 @@ func (s *State) Encode() []byte {
 	baseline := s.Baseline()
 	current := s.Current()
 
-	var bits bits
-	bits.Update(baseline, current)
-	bits.WriteTo(w)
-
 	for i, cube := range current.Cubes {
 		base := baseline.Cubes[i]
+
+		//bits := deltabits(&base, &cube)
+		bits := uint64(32)
+
+		//w.WriteBits(bits, 6)
+		//if bits == 0 {
+		//	continue
+		//}
+
+		//if bits == 0 {
+		//	w.WriteBit(0)
+		//	continue
+		//}
+		//w.WriteBit(1)
+		//w.WriteBits(bits, 6)
+		//expgolomb.WriteInt(w, int(bits)-9)
 
 		write(w, cube.Interacting^base.Interacting, 1)
 		write(w, cube.Largest^base.Largest, 2)
 
-		write32(w, cube.A^base.A, bits.ABC)
-		write32(w, cube.B^base.B, bits.ABC)
-		write32(w, cube.C^base.C, bits.ABC)
-
-		write32(w, cube.X^base.X, bits.XYZ)
-		write32(w, cube.Y^base.Y, bits.XYZ)
-		write32(w, cube.Z^base.Z, bits.XYZ)
+		write32(w, cube.A^base.A, bits)
+		write32(w, cube.B^base.B, bits)
+		write32(w, cube.C^base.C, bits)
+		write32(w, cube.X^base.X, bits)
+		write32(w, cube.Y^base.Y, bits)
+		write32(w, cube.Z^base.Z, bits)
 	}
 
 	w.Close()
@@ -108,22 +105,31 @@ func (s *State) Decode(snapshot []byte) {
 	baseline := s.Baseline()
 	current := s.Current()
 
-	var bits bits
-	bits.ReadFrom(r)
-
 	for i := range current.Cubes {
 		base := baseline.Cubes[i]
 		cube := &current.Cubes[i]
 
+		bits := uint64(32)
+		//bits, _ := r.ReadBits(6)
+		//if bits == 0 {
+		//	continue
+		//}
+
+		//xbits, _ := expgolomb.ReadInt(r)
+		//if xbits == 0 {
+		//	continue
+		//}
+		//bits := uint64(xbits + 9)
+
 		cube.Interacting = read(r, 1) ^ base.Interacting
 		cube.Largest = read(r, 2) ^ base.Largest
 
-		cube.A = read32(r, bits.ABC) ^ base.A
-		cube.B = read32(r, bits.ABC) ^ base.B
-		cube.C = read32(r, bits.ABC) ^ base.C
+		cube.A = read32(r, bits) ^ base.A
+		cube.B = read32(r, bits) ^ base.B
+		cube.C = read32(r, bits) ^ base.C
 
-		cube.X = read32(r, bits.XYZ) ^ base.X
-		cube.Y = read32(r, bits.XYZ) ^ base.Y
-		cube.Z = read32(r, bits.XYZ) ^ base.Z
+		cube.X = read32(r, bits) ^ base.X
+		cube.Y = read32(r, bits) ^ base.Y
+		cube.Z = read32(r, bits) ^ base.Z
 	}
 }
