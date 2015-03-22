fac2811dd7f0df5bd8ae1d9561519e336e8b5315
Egon Elbre <egonelbre@gmail.com>
2015-03-22 16:27:20 +0200

    Fix ZigZag.

--- bit/zigzag.go
+++ bit/zigzag.go
@@ -4,12 +4,15 @@ package bit
 // The sign will be encoded as the least significant bit in the result
 // ZEncode(x) < abs(x)*2
 func ZEncode(x int64) uint64 {
-	ux := uint64(x)
-	return (ux << 1) ^ (ux >> 63)
+	return uint64((x << 1) ^ (x >> 63))
 }
 
 // ZDecode is the inverse operation of ZEncode
 // ZDecode(ZEncode(x)) == x
 func ZDecode(ux uint64) int64 {
-	return int64((ux >> 1) ^ (ux << 63))
+	//TODO: optimize
+	if ux&1 == 1 {
+		return -int64(ux>>1 + 1)
+	}
+	return int64(ux >> 1)
 }
--- bit/zigzag_test.go
+++ bit/zigzag_test.go
@@ -6,12 +6,15 @@ import (
 )
 
 func TestZigRandom(t *testing.T) {
-	for i := 0; i < 1521; i += 1 {
-		x := rand.Int63() * (rand.Int63n(2)*2 - 1)
+	for i := 0; i < 15315; i += 1 {
+		x := rand.Int63()
+		if rand.Intn(2) == 1 {
+			x = -x
+		}
 		rx := ZEncode(x)
 		rrx := ZDecode(rx)
 		if rrx != x {
-			t.Errorf("failed %v: %v / %v", x, rx, rrx)
+			t.Fatalf("failed %v: %v / %v", x, rx, rrx)
 		}
 	}
 }
