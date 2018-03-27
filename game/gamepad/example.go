729764384f335ae1a9c920690f0cd91dfc2b91a5
Egon Elbre <egonelbre@gmail.com>
2018-03-27 14:56:05 +0300

    Add gamepad example

---
+++ game/gamepad/example.go
@@ -0,0 +1,30 @@
+// +build ignore
+
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/loov/input/gamepad"
+)
+
+func main() {
+	gamepads := gamepad.All{}
+
+	prev := int16(0)
+	for range time.Tick(1 * time.Millisecond) {
+		gamepads.Update()
+		for i := range gamepads {
+			pad := &gamepads[i]
+			if !pad.Connected {
+				continue
+			}
+
+			if pad.Raw.ThumbLX != prev {
+				fmt.Println(time.Now().Nanosecond(), pad.Raw.ThumbLX, pad.Raw.ThumbRX)
+				prev = pad.Raw.ThumbLX
+			}
+		}
+	}
+}
