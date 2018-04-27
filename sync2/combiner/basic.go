5afc6f594686ee302ab51ca7863d586b2ad33a16
Egon Elbre <egonelbre@gmail.com>
2018-04-27 23:55:14 +0300

    sync2/combiner: use uintptr instead of unsafe.Pointer

--- sync2/combiner/basic.go
+++ sync2/combiner/basic.go
@@ -9,24 +9,22 @@ import (
 // based on https://software.intel.com/en-us/blogs/2013/02/22/combineraggregator-synchronization-primitive
 type Basic struct {
 	exe  Executor
-	head unsafe.Pointer // *basicArg
+	head uintptr // *basicArg
 }
 
 type basicArg struct {
 	value int64
-	next  unsafe.Pointer // *basicArg
+	next  uintptr // *basicArg
 }
 
 func NewBasic(exe Executor) *Basic {
 	return &Basic{
 		exe:  exe,
-		head: nil,
+		head: 0,
 	}
 }
 
-var basicLockedElem = basicArg{}
-var basicLockedArg = &basicLockedElem
-var basicLocked = (unsafe.Pointer)(basicLockedArg)
+const basicLocked = uintptr(1)
 
 func (c *Basic) Do(v int64) {
 	arg := &basicArg{value: v}
@@ -45,28 +43,28 @@ func (c *Basic) Do(v int64) {
 	// 1. If c.head == nil, exchange it to LOCKED and become the combiner.
 	// Otherwise, enqueue own arg into the c->head lock-free list.
 
-	var cmp unsafe.Pointer
+	var cmp uintptr
 	for {
-		cmp = atomic.LoadPointer(&c.head)
+		cmp = atomic.LoadUintptr(&c.head)
 		xchg := basicLocked
-		if cmp != nil {
+		if cmp != 0 {
 			// There is already a combiner, enqueue itself.
-			xchg = (unsafe.Pointer)(arg)
+			xchg = uintptr(unsafe.Pointer(arg))
 			arg.next = cmp
 		}
 
-		if atomic.CompareAndSwapPointer(&c.head, cmp, xchg) {
+		if atomic.CompareAndSwapUintptr(&c.head, cmp, xchg) {
 			break
 		}
 	}
 
-	if cmp != nil {
+	if cmp != 0 {
 		// 2. If we are not the combiner, wait for arg.next to become nil
 		// (which means the operation is finished).
 
 		const maxspin = 256
 		spin := 0
-		for atomic.LoadPointer(&arg.next) != nil {
+		for atomic.LoadUintptr(&arg.next) != 0 {
 			spin++
 			if spin >= maxspin {
 				runtime.Gosched()
@@ -85,16 +83,16 @@ func (c *Basic) Do(v int64) {
 		// Then, look for combining opportunities.
 		for {
 			for {
-				cmp = atomic.LoadPointer(&c.head)
+				cmp = atomic.LoadUintptr(&c.head)
 				// If there are some operations in the list,
 				// grab the list and replace with LOCKED.
 				// Otherwise, exchange to nil.
-				var xchg unsafe.Pointer = nil
+				var xchg uintptr = 0
 				if cmp != basicLocked {
 					xchg = basicLocked
 				}
 
-				if atomic.CompareAndSwapPointer(&c.head, cmp, xchg) {
+				if atomic.CompareAndSwapUintptr(&c.head, cmp, xchg) {
 					break
 				}
 			}
@@ -104,14 +102,14 @@ func (c *Basic) Do(v int64) {
 				break
 			}
 
-			arg = (*basicArg)(cmp)
 			// Execute the list of operations.
-			for arg != basicLockedArg {
-				next := (*basicArg)(arg.next)
+			for cmp != basicLocked {
+				arg = (*basicArg)(unsafe.Pointer(cmp))
+				cmp = arg.next
+
 				c.exe.Include(arg.value)
 				// Mark completion.
-				atomic.StorePointer(&arg.next, nil)
-				arg = next
+				atomic.StoreUintptr(&arg.next, 0)
 			}
 		}
 	}
