08b116108ae85f39249ed5f9e2fcda23add15b79
Egon Elbre <egonelbre@gmail.com>
2018-04-29 01:43:56 +0300

    queue: nicer implementation of nextPowerOfTwo

--- queue/util.go
+++ queue/util.go
@@ -1,5 +1,7 @@
 package queue
 
+import "math/bits"
+
 func ceil(a, n int) int {
 	r := ((a + n - 1) / n) * n
 	if r <= n {
@@ -9,12 +11,5 @@ func ceil(a, n int) int {
 }
 
 func nextPowerOfTwo(v uint32) uint32 {
-	v--
-	v |= v >> 1
-	v |= v >> 2
-	v |= v >> 4
-	v |= v >> 8
-	v |= v >> 16
-	v++
-	return v
+	return 1 << uint(32-bits.LeadingZeros32(v))
 }
