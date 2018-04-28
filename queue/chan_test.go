e96c2a1c3baba18b67459c1ca64147a44624b66f
Egon Elbre <egonelbre@gmail.com>
2018-04-28 17:02:53 +0300

    queue: add benchmarks for SPSC

--- queue/chan_test.go
+++ queue/chan_test.go
@@ -5,3 +5,7 @@ import "testing"
 func TestChan(t *testing.T) {
 	test(t, func(size int) Queue { return NewChan(size) })
 }
+
+func BenchmarkChan(b *testing.B) {
+	bench(b, func(size int) Queue { return NewChan(size) })
+}
--- queue/queue_test.go
+++ queue/queue_test.go
@@ -3,6 +3,7 @@ package queue
 import (
 	"fmt"
 	"runtime"
+	"sync"
 	"sync/atomic"
 	"testing"
 	"time"
@@ -433,5 +434,116 @@ func testNonblockingMPMC(t *testing.T, ctor func(int) NonblockingMPMC) {
 }
 
 func bench(b *testing.B, ctor Ctor) {
+	benchCommon(b, ctor)
+
+	b.Run("b", func(b *testing.B) {
+		if ctor := ctor.BlockingSPSC(); ctor != nil {
+			b.Run("SPSC", func(b *testing.B) { benchBlockingSPSC(b, ctor) })
+		}
+		if ctor := ctor.BlockingSPMC(); ctor != nil {
+			b.Run("SPMC", func(b *testing.B) { benchBlockingSPMC(b, ctor) })
+		}
+		if ctor := ctor.BlockingMPSC(); ctor != nil {
+			b.Run("MPSC", func(b *testing.B) { benchBlockingMPSC(b, ctor) })
+		}
+		if ctor := ctor.BlockingMPMC(); ctor != nil {
+			b.Run("MPMC", func(b *testing.B) { benchBlockingMPMC(b, ctor) })
+		}
+	})
+
+	b.Run("n", func(b *testing.B) {
+		if ctor := ctor.NonblockingSPSC(); ctor != nil {
+			b.Run("SPSC", func(b *testing.B) { benchNonblockingSPSC(b, ctor) })
+		}
+		if ctor := ctor.NonblockingSPMC(); ctor != nil {
+			b.Run("SPMC", func(b *testing.B) { benchNonblockingSPMC(b, ctor) })
+		}
+		if ctor := ctor.NonblockingMPSC(); ctor != nil {
+			b.Run("MPSC", func(b *testing.B) { benchNonblockingMPSC(b, ctor) })
+		}
+		if ctor := ctor.NonblockingMPMC(); ctor != nil {
+			b.Run("MPMC", func(b *testing.B) { benchNonblockingMPMC(b, ctor) })
+		}
+	})
+}
+
+func benchCommon(b *testing.B, ctor func(int) Queue) {
+	b.Run("Create", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			_ = ctor(TestSizes[i%len(TestSizes)])
+		}
+	})
+}
+
+func benchBlockingSPSC(b *testing.B, ctor func(int) BlockingSPSC) {
+	b.Run("Single", func(b *testing.B) {
+		q := ctor(TestSize)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			var v Value
+			q.Send(v)
+			q.Recv(&v)
+		}
+	})
+	b.Run("Parallel100", func(b *testing.B) {
+		b.RunParallel(func(pb *testing.PB) {
+			q := ctor(TestSize)
+			for pb.Next() {
+				var v Value
+				for i := 0; i < 100; i++ {
+					q.Send(v)
+					q.Recv(&v)
+				}
+			}
+		})
+	})
+	b.Run("Uncontended", func(b *testing.B) {
+		q := ctor(TestSize)
+		b.ResetTimer()
+		var wg sync.WaitGroup
+		wg.Add(2)
+		go func() {
+			for i := 0; i < b.N; i++ {
+				var v Value
+				q.Send(v)
+			}
+			wg.Done()
+		}()
+		go func() {
+			for i := 0; i < b.N; i++ {
+				var v Value
+				q.Recv(&v)
+			}
+			wg.Done()
+		}()
+		wg.Wait()
+	})
 
 }
+func benchBlockingMPSC(b *testing.B, ctor func(int) BlockingMPSC) { b.Skip("todo") }
+func benchBlockingSPMC(b *testing.B, ctor func(int) BlockingSPMC) { b.Skip("todo") }
+func benchBlockingMPMC(b *testing.B, ctor func(int) BlockingMPMC) { b.Skip("todo") }
+func benchNonblockingSPSC(b *testing.B, ctor func(int) NonblockingSPSC) {
+	b.Run("Single", func(b *testing.B) {
+		q := ctor(TestSize)
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			var v Value
+			q.TrySend(v)
+			q.TryRecv(&v)
+		}
+	})
+	b.Run("Parallel", func(b *testing.B) {
+		b.RunParallel(func(pb *testing.PB) {
+			q := ctor(TestSize)
+			for pb.Next() {
+				var v Value
+				q.TrySend(v)
+				q.TryRecv(&v)
+			}
+		})
+	})
+}
+func benchNonblockingMPSC(b *testing.B, ctor func(int) NonblockingMPSC) { b.Skip("todo") }
+func benchNonblockingSPMC(b *testing.B, ctor func(int) NonblockingSPMC) { b.Skip("todo") }
+func benchNonblockingMPMC(b *testing.B, ctor func(int) NonblockingMPMC) { b.Skip("todo") }
