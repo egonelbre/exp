9d78df6a56e47fba2a8077346ba7e8b47f526382
Egon Elbre <egonelbre@gmail.com>
2018-05-10 21:32:52 +0300

    combiner: fix tests

--- combiner/all_test.go
+++ combiner/all_test.go
@@ -1,7 +1,19 @@
 package combiner
 
-import "testing"
+import (
+	"flag"
+	"testing"
+)
 
-func Test(t *testing.T)        { All.TestDefault(t) }
-func TestLatency(t *testing.T) { All.LatencyDefault(t) }
-func Benchmark(b *testing.B)   { All.BenchmarkDefault(b) }
+func Test(t *testing.T) { All.TestDefault(t) }
+
+var latency = flag.Bool("latency", false, "run latency measurements")
+
+func TestLatency(t *testing.T) {
+	if !*latency {
+		t.Skip("latency measurements disabled")
+	}
+	All.LatencyDefault(t)
+}
+
+func Benchmark(b *testing.B) { All.BenchmarkDefault(b) }
