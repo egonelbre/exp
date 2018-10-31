70c41477e5b7a86048ec7493feb30a9557b3e702
Egon Elbre <egonelbre@gmail.com>
2018-10-31 11:00:13 +0200

    Add benchmark for cgocheck

--- bench/call/call_test.go
+++ bench/call/call_test.go
@@ -6,15 +6,15 @@ import (
 	"github.com/egonelbre/exp/bench/call/asm"
 )
 
-func BenchmarkCGO(b *testing.B) {
+func BenchmarkNopGo(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		CNop()
+		Nop()
 	}
 }
 
-func BenchmarkGo(b *testing.B) {
+func BenchmarkNopCGO(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Nop()
+		CNop()
 	}
 }
 
@@ -22,4 +22,94 @@ func BenchmarkAsm(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		asm.Nop()
 	}
+}
+
+func BenchmarkArgs1(b *testing.B) {
+	n := NewDeviceCreateInfo
+	x0 := n()
+	for i := 0; i < b.N; i++ {
+		Args1(x0)
+	}
+	Release(x0)
+}
+
+func BenchmarkArgs2(b *testing.B) {
+	n := NewDeviceCreateInfo
+	x0, x1 := n(), n()
+	for i := 0; i < b.N; i++ {
+		Args2(x0, x1)
+	}
+	Release(x0, x1)
+}
+
+func BenchmarkArgs3(b *testing.B) {
+	n := NewDeviceCreateInfo
+	x0, x1, x2 := n(), n(), n()
+	for i := 0; i < b.N; i++ {
+		Args3(x0, x1, x2)
+	}
+	Release(x0, x1, x2)
+}
+
+func BenchmarkArgs4(b *testing.B) {
+	n := NewDeviceCreateInfo
+	x0, x1, x2, x3 := n(), n(), n(), n()
+	for i := 0; i < b.N; i++ {
+		Args4(x0, x1, x2, x3)
+	}
+	Release(x0, x1, x2, x3)
+}
+
+func BenchmarkArgs8(b *testing.B) {
+	n := NewDeviceCreateInfo
+	x0, x1, x2, x3, x4, x5, x6, x7 := n(), n(), n(), n(), n(), n(), n(), n()
+	for i := 0; i < b.N; i++ {
+		Args8(x0, x1, x2, x3, x4, x5, x6, x7)
+	}
+	Release(x0, x1, x2, x3, x4, x5, x6, x7)
+}
+
+func BenchmarkCArgs1(b *testing.B) {
+	n := NewDeviceCreateInfo
+	x0 := n()
+	for i := 0; i < b.N; i++ {
+		CArgs1(x0)
+	}
+	Release(x0)
+}
+
+func BenchmarkCArgs2(b *testing.B) {
+	n := NewDeviceCreateInfo
+	x0, x1 := n(), n()
+	for i := 0; i < b.N; i++ {
+		CArgs2(x0, x1)
+	}
+	Release(x0, x1)
+}
+
+func BenchmarkCArgs3(b *testing.B) {
+	n := NewDeviceCreateInfo
+	x0, x1, x2 := n(), n(), n()
+	for i := 0; i < b.N; i++ {
+		CArgs3(x0, x1, x2)
+	}
+	Release(x0, x1, x2)
+}
+
+func BenchmarkCArgs4(b *testing.B) {
+	n := NewDeviceCreateInfo
+	x0, x1, x2, x3 := n(), n(), n(), n()
+	for i := 0; i < b.N; i++ {
+		CArgs4(x0, x1, x2, x3)
+	}
+	Release(x0, x1, x2, x3)
+}
+
+func BenchmarkCArgs8(b *testing.B) {
+	n := NewDeviceCreateInfo
+	x0, x1, x2, x3, x4, x5, x6, x7 := n(), n(), n(), n(), n(), n(), n(), n()
+	for i := 0; i < b.N; i++ {
+		CArgs8(x0, x1, x2, x3, x4, x5, x6, x7)
+	}
+	Release(x0, x1, x2, x3, x4, x5, x6, x7)
 }
--- bench/call/cgo.go
+++ bench/call/cgo.go
@@ -1,9 +1,110 @@
 package call
 
-//void nop() { }
+import "unsafe"
+
+/*
+#include <stdint.h>
+#include <stdlib.h>
+
+// Vulkan like data structures
+
+typedef uint32_t VkFlags;
+typedef VkFlags  VkDeviceQueueCreateFlags;
+typedef uint32_t VkStructureType;
+
+typedef struct VkDeviceQueueCreateInfo {
+    VkStructureType             sType;
+    const void*                 pNext;
+    VkDeviceQueueCreateFlags    flags;
+    uint32_t                    queueFamilyIndex;
+    uint32_t                    queueCount;
+    const float*                pQueuePriorities;
+} VkDeviceQueueCreateInfo;
+
+typedef struct VkPhysicalDeviceFeatures {
+    uint32_t bools[56];
+} VkPhysicalDeviceFeatures;
+
+typedef struct VkDeviceCreateInfo {
+    VkStructureType                    sType;
+    const void*                        pNext;
+    VkFlags                            flags;
+    uint32_t                           queueCreateInfoCount;
+    const VkDeviceQueueCreateInfo*     pQueueCreateInfos;
+    uint32_t                           enabledLayerCount;
+    const char* const*                 ppEnabledLayerNames;
+    uint32_t                           enabledExtensionCount;
+    const char* const*                 ppEnabledExtensionNames;
+    const VkPhysicalDeviceFeatures*    pEnabledFeatures;
+} VkDeviceCreateInfo;
+
+void nop() { }
+
+VkDeviceCreateInfo * createInfo() {
+	VkDeviceCreateInfo *info = malloc(sizeof(VkDeviceCreateInfo));
+	VkDeviceQueueCreateInfo *queue = malloc(sizeof(VkDeviceQueueCreateInfo));
+	info->pQueueCreateInfos = queue;
+	return info;
+}
+
+void create1(VkDeviceCreateInfo *x0) {}
+void create2(VkDeviceCreateInfo *x0, VkDeviceCreateInfo *x1) {}
+void create3(VkDeviceCreateInfo *x0, VkDeviceCreateInfo *x1, VkDeviceCreateInfo *x2) {}
+void create4(VkDeviceCreateInfo *x0, VkDeviceCreateInfo *x1, VkDeviceCreateInfo *x2, VkDeviceCreateInfo *x3) {}
+void create8(
+	VkDeviceCreateInfo *x0, VkDeviceCreateInfo *x1, VkDeviceCreateInfo *x2, VkDeviceCreateInfo *x3,
+	VkDeviceCreateInfo *x4, VkDeviceCreateInfo *x5, VkDeviceCreateInfo *x6, VkDeviceCreateInfo *x7
+) {}
+*/
 import "C"
 
 //go:noinline
 func Nop() {}
 
 func CNop() { C.nop() }
+
+func NewDeviceCreateInfo() *C.VkDeviceCreateInfo {
+	return C.createInfo()
+}
+
+func Release(xs ...*C.VkDeviceCreateInfo)  {
+	for _, x := range xs {
+		C.free(unsafe.Pointer(x))
+	}
+}
+
+//go:noinline
+func Args1(x0 *C.VkDeviceCreateInfo) {	}
+
+//go:noinline
+func Args2(x0, x1 *C.VkDeviceCreateInfo) {}
+
+//go:noinline
+func Args3(x0, x1, x2 *C.VkDeviceCreateInfo) {}
+
+//go:noinline
+func Args4(x0, x1, x2, x3 *C.VkDeviceCreateInfo) {}
+
+//go:noinline
+func Args8(x0, x1, x2, x3, x4, x5, x6, x7 *C.VkDeviceCreateInfo) {}
+
+func CArgs1(x0 *C.VkDeviceCreateInfo) {	
+	C.create1(x0)
+}
+
+func CArgs2(x0, x1 *C.VkDeviceCreateInfo) {
+	C.create2(x0, x1)
+}
+
+func CArgs3(x0, x1, x2 *C.VkDeviceCreateInfo) {
+	C.create3(x0, x1, x2)
+}
+
+func CArgs4(x0, x1, x2, x3 *C.VkDeviceCreateInfo) {
+	C.create4(x0, x1, x2, x3)
+}
+
+func CArgs8(x0, x1, x2, x3, x4, x5, x6, x7 *C.VkDeviceCreateInfo) {
+	C.create8(x0, x1, x2, x3, x4, x5, x6, x7)
+}
+
