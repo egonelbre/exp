2719021729830cfd3c2a989d90b66e9bd8000dfe
Egon Elbre <egonelbre@gmail.com>
2018-11-23 16:13:41 +0200

    bench/iface2: add comparison with nop

--- bench/iface2/main.go
+++ bench/iface2/main.go
@@ -10,8 +10,8 @@ import (
 func main() {
 	runtime.LockOSThread()
 
-	const N = 10000000
-	const ProbablyGC = 150
+	const N = 100000000
+	const ProbablyGC = 100
 
 	var overhead uint64
 	var overheadCount int
@@ -26,7 +26,7 @@ func main() {
 	}
 
 	averageOverhead := float64(overhead) / float64(overheadCount)
-	fmt.Println("Average overhad:", averageOverhead, "cy")
+	fmt.Println("measurement overhead:", averageOverhead, "cy")
 
 	{
 		var measurement uint64
@@ -52,6 +52,31 @@ func main() {
 		averageCall := float64(measurement) / float64(measurementCount)
 		fmt.Println("iface call:", averageCall-averageOverhead, "cy")
 	}
+
+
+	{
+		var measurement uint64
+		var measurementCount int
+		for i := 0; i < N; i++ {
+			start := hrtime.RDTSC()
+			nop()
+			stop := hrtime.RDTSCP()
+			delta := stop - start
+			if delta < ProbablyGC {
+				measurement += delta
+				measurementCount++
+			}
+
+			if i & 1 == 1 {
+				example = &A{}
+			} else {
+				example = &B{}
+			}
+		}
+
+		averageCall := float64(measurement) / float64(measurementCount)
+		fmt.Println("nop call:", averageCall-averageOverhead, "cy")
+	}
 }
 
 var example Interface = &A{}
@@ -60,8 +85,12 @@ type Interface interface {
 	Get() int
 }
 
-type A struct{ Value int }
-type B struct{ Value int }
+type A struct{ _ int; Value int }
+type B struct{ _ int; Value int }
 
 func (x *A) Get() int { return x.Value }
 func (x *B) Get() int { return x.Value }
+
+
+//go:noinline
+func nop() int { return 0 }
