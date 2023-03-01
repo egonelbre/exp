f6450477ca8db59667a8e236ca95f9d5b9fbb21e
Egon Elbre <egonelbre@gmail.com>
2023-03-01 19:55:43 +0200

    bench/maplookup: add binary search

--- bench/maplookup/lookup_test.go
+++ bench/maplookup/lookup_test.go
@@ -19,6 +19,23 @@ func (q LookupList) Contains(x string) bool {
 	return false
 }
 
+type BinaryList []string
+
+func (x BinaryList) Contains(target string) bool {
+	n := len(x)
+	i, j := 0, n
+	for i < j {
+		h := int(uint(i+j) >> 1) 
+		if x[h] < target {
+			i = h + 1 
+		} else {
+			j = h 
+		}
+	}
+	
+	return i < n && x[i] == target
+}
+
 func lookupList(entries []string) LookupList {
 	sort.Strings(entries)
 	full := strings.Join(entries, "")
@@ -28,6 +45,10 @@ func lookupList(entries []string) LookupList {
 		full = full[len(s):]
 	}
 	return LookupList(list)
+}
+
+func binaryList(entries []string) BinaryList {
+	return BinaryList(lookupList(entries))
 }
 
 type LookupMap map[string]struct{}
@@ -47,7 +68,7 @@ func lookupMap(entries []string) LookupMap {
 }
 
 var randomStrings []string
-var sizes = []int{1, 4, 8, 10, 12, 14, 16, 18, 24}
+var sizes = []int{1, 4, 6, 8, 10, 12, 14, 16, 18, 24}
 
 const maxLookup = 10000
 
@@ -96,33 +117,48 @@ func scenarios(targets, mismatches []string) []scenario {
 	}
 }
 
-var z bool
+var z int
 
 func Benchmark(b *testing.B) {
 	for _, size := range sizes {
 		targets := randomStrings[:size]
 		mismatches := randomStrings[size:]
 
-		list := lookupList(targets)
+		lisx := lookupList(targets)
+		binx := binaryList(targets)
 		mapx := lookupMap(targets)
 
 		for _, scenario := range scenarios(targets, mismatches) {
 			b.Run(fmt.Sprintf("Lis/%d-%s", size, scenario.name), func(b *testing.B) {
 				for i := 0; i < b.N; i++ {
 					for _, q := range scenario.query {
-						z = list.Contains(q)
+						if lisx.Contains(q) {
+							z++
+						}
+					}
+				}
+			})
+			b.Run(fmt.Sprintf("Bin/%d-%s", size, scenario.name), func(b *testing.B) {
+				for i := 0; i < b.N; i++ {
+					for _, q := range scenario.query {
+						if binx.Contains(q) {
+							z++
+						}
 					}
 				}
 			})
 			b.Run(fmt.Sprintf("Map/%d-%s", size, scenario.name), func(b *testing.B) {
 				for i := 0; i < b.N; i++ {
 					for _, q := range scenario.query {
-						z = mapx.Contains(q)
+						if mapx.Contains(q) {
+							z++
+						}
 					}
 				}
 			})
 		}
 	}
+	b.Log(z)
 }
 
 func concat(xs ...[]string) []string {
