7289e8c02dbf608a9aa2d2320906b84c0a15a176
Egon Elbre <egonelbre@gmail.com>
2024-01-24 18:14:18 +0200

    compare: more implementations

--- vector/compare/axpy_arm64.go
+++ vector/compare/axpy_arm64.go
@@ -6,6 +6,8 @@ func ArmAxpyPointer(alpha float32, xs *float32, incx uintptr, ys *float32, incy 
 func ArmAxpyPointerLoop(alpha float32, xs *float32, incx uintptr, ys *float32, incy uintptr, n uintptr)
 func ArmAxpyPointerLoopX(alpha float32, xs *float32, incx uintptr, ys *float32, incy uintptr, n uintptr)
 func ArmAxpyUnsafeXR4(alpha float32, xs *float32, incx uintptr, ys *float32, incy uintptr, n uintptr)
+func ArmAxpyUnsafeInterleaveXR4(alpha float32, xs *float32, incx uintptr, ys *float32, incy uintptr, n uintptr)
+func ArmAxpyPointerLoopXR4(alpha float32, xs *float32, incx uintptr, ys *float32, incy uintptr, n uintptr)
 
 type armAxpyDecl struct {
 	name string
@@ -18,5 +20,7 @@ var armAxpyDecls = []armAxpyDecl{
 	{name: "AxpyPointer", fn: ArmAxpyPointer},
 	{name: "AxpyPointerLoop", fn: ArmAxpyPointerLoop},
 	{name: "AxpyPointerLoopX", fn: ArmAxpyPointerLoopX},
-	{name: "ArmAxpyUnsafeXR4", fn: ArmAxpyUnsafeXR4},
+	{name: "AxpyUnsafeXR4", fn: ArmAxpyUnsafeXR4},
+	{name: "AxpyUnsafeInterleaveXR4", fn: ArmAxpyUnsafeInterleaveXR4},
+	{name: "AxpyPointerLoopXR4", fn: ArmAxpyPointerLoopXR4},
 }
--- vector/compare/axpy_arm64.s
+++ vector/compare/axpy_arm64.s
@@ -178,7 +178,7 @@ loop_unroll:
 
 	ADD  R6, R1, R6 // xi += incx
 	ADD  R7, R3, R7 // yi += incy
-    FMOVS  (R0)(R6<<2), F1 // f1 = xs[xi]
+	FMOVS  (R0)(R6<<2), F1 // f1 = xs[xi]
 	FMOVS  (R2)(R7<<2), F2 // f2 = ys[yi]
 	FMADDS F0, F2, F1, F2  // f1 = alpha * f1 + f2
 	FMOVS  F2, (R2)(R7<<2) // ys[yi] = f1
@@ -219,4 +219,139 @@ loop:
 
 check_limit:
 	CBNZ R4, loop
+	RET
+
+// func ArmAxpyUnsafeInterleaveXR4(alpha float32, xs *float32, incx uintptr, ys *float32, incy uintptr, n uintptr)
+TEXT ·ArmAxpyUnsafeInterleaveXR4(SB), NOSPLIT, $0-48
+	FMOVS alpha+0(FP), F0
+
+	MOVD  xs+8(FP),    R0
+	MOVD  incx+16(FP), R1
+
+	MOVD  ys+24(FP),   R2
+	MOVD  incy+32(FP), R3
+
+	MOVD  n+40(FP), R4 // n
+
+	MOVD ZR, R6 // xi
+	MOVD ZR, R7 // yi
+
+	JMP  check_limit_unroll
+
+loop_unroll:
+	FMOVS  (R0)(R6<<2), F1 // f1 = xs[xi]
+	ADD  R6, R1, R6 // xi += incx
+	FMOVS  (R0)(R6<<2), F2 // f1 = xs[xi]
+	ADD  R6, R1, R6 // xi += incx
+	FMOVS  (R0)(R6<<2), F3 // f1 = xs[xi]
+	ADD  R6, R1, R6 // xi += incx
+	FMOVS  (R0)(R6<<2), F4 // f1 = xs[xi]
+	ADD  R6, R1, R6 // xi += incx
+
+	FMOVS  (R2)(R7<<2), F5 // f2 = ys[yi]
+	FMADDS F0, F5, F1, F5  // f1 = alpha * f1 + f2
+	FMOVS  F5, (R2)(R7<<2) // ys[yi] = f1
+	ADD  R7, R3, R7 // yi += incy
+
+	FMOVS  (R2)(R7<<2), F5 // f2 = ys[yi]
+	FMADDS F0, F5, F2, F5  // f1 = alpha * f1 + f2
+	FMOVS  F5, (R2)(R7<<2) // ys[yi] = f1
+	ADD  R7, R3, R7 // yi += incy
+
+	FMOVS  (R2)(R7<<2), F5 // f2 = ys[yi]
+	FMADDS F0, F5, F3, F5  // f1 = alpha * f1 + f2
+	FMOVS  F5, (R2)(R7<<2) // ys[yi] = f1
+	ADD  R7, R3, R7 // yi += incy
+
+	FMOVS  (R2)(R7<<2), F5 // f2 = ys[yi]
+	FMADDS F0, F5, F4, F5  // f1 = alpha * f1 + f2
+	FMOVS  F5, (R2)(R7<<2) // ys[yi] = f1
+	ADD  R7, R3, R7 // yi += incy
+
+	SUB  $4, R4, R4    // n -= 4
+
+check_limit_unroll:
+	CMP  $0x04, R4
+	BHS  loop_unroll
+	JMP  check_limit
+
+loop:
+	FMOVS  (R0)(R6<<2), F1 // f1 = xs[xi]
+	FMOVS  (R2)(R7<<2), F2 // f2 = ys[yi]
+	// FMADDS A, B, C, D ==> D := A*C + B
+	FMADDS F0, F2, F1, F2  // f1 = alpha * xs[xi] + ys[yi]
+	FMOVS  F2, (R2)(R7<<2) // ys[yi] = f1
+
+	SUB  $1, R4, R4 // n--
+	ADD  R6, R1, R6 // xi += incx
+	ADD  R7, R3, R7 // yi += incy
+
+check_limit:
+	CBNZ R4, loop
+	RET
+
+
+// func ArmAxpyPointerLoopXR4(alpha float32, xs *float32, incx uintptr, ys *float32, incy uintptr, n uintptr)
+TEXT ·ArmAxpyPointerLoopXR4(SB), NOSPLIT, $0-48
+	FMOVS alpha+0(FP), F0
+
+	MOVD  xs+8(FP),    R0
+	MOVD  incx+16(FP), R1
+
+	MOVD  ys+24(FP),   R2
+	MOVD  incy+32(FP), R3
+
+	MOVD  n+40(FP), R4
+
+	JMP   check_limit_unroll
+
+loop_unroll:
+	FMOVS  (R0), F1        // f1 = *xs
+	FMOVS  (R2), F2        // f2 = *ys
+	FMADDS F0, F2, F1, F2  // f1 = alpha * *xs + *ys
+	FMOVS  F2, (R2)        // *ys = f1
+	ADD    R1<<2, R0, R0   // xs += incx * 4
+	ADD    R3<<2, R2, R2   // ys += incy * 4
+
+	FMOVS  (R0), F1        // f1 = *xs
+	FMOVS  (R2), F2        // f2 = *ys
+	FMADDS F0, F2, F1, F2  // f1 = alpha * *xs + *ys
+	FMOVS  F2, (R2)        // *ys = f1
+	ADD    R1<<2, R0, R0   // xs += incx * 4
+	ADD    R3<<2, R2, R2   // ys += incy * 4
+
+	FMOVS  (R0), F1        // f1 = *xs
+	FMOVS  (R2), F2        // f2 = *ys
+	FMADDS F0, F2, F1, F2  // f1 = alpha * *xs + *ys
+	FMOVS  F2, (R2)        // *ys = f1
+	ADD    R1<<2, R0, R0   // xs += incx * 4
+	ADD    R3<<2, R2, R2   // ys += incy * 4
+
+	FMOVS  (R0), F1        // f1 = *xs
+	FMOVS  (R2), F2        // f2 = *ys
+	FMADDS F0, F2, F1, F2  // f1 = alpha * *xs + *ys
+	FMOVS  F2, (R2)        // *ys = f1
+	ADD    R1<<2, R0, R0   // xs += incx * 4
+	ADD    R3<<2, R2, R2   // ys += incy * 4
+
+	SUB  $4, R4, R4
+
+check_limit_unroll:
+	CMP  $0x04, R4
+	BHS  loop_unroll
+	JMP   check_limit
+
+loop:
+	FMOVS  (R0), F1        // f1 = *xs
+	FMOVS  (R2), F2        // f2 = *ys
+	FMADDS F0, F2, F1, F2  // f1 = alpha * *xs + *ys
+	FMOVS  F2, (R2)        // *ys = f1
+	ADD    R1<<2, R0, R0   // xs += incx * 4
+	ADD    R3<<2, R2, R2   // ys += incy * 4
+
+	SUB $1, R4, R4
+
+check_limit:
+	CBNZ  R4, loop
+
 	RET
